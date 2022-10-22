c3bd819f0357643e19bd69fb388fcefc2078d3ba
b_l_a_in <J>
2022-10-22 09:13:37 +0300

    task 392

---
+++ 392. Is Subsequence/392_sol.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s_index = 0
+        t_index = 0
+        if s == '':
+            return True
+        while t_index<len(t):
+            if s[s_index] == t[t_index]:
+                s_index += 1
+            t_index += 1
+            if s_index == len(s):
+                return True
+        return False
+        
+        
